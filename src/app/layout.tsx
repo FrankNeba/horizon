651ad1f4e8a2a993a0f1338a567b3e34ecdf220e
FrankNeba <ngwafranklin29@gmail.com>
2026-09-17 11:51:29 +0100

    smartsupp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <Script id="smartsupp-script" strategy="afterInteractive">
           {`
             var _smartsupp = _smartsupp || {};
-            _smartsupp.key = 'your-smartsupp-key';
+            _smartsupp.key = '2e1dfd66b8e2d01c762627296af365282c437c42';
             window.smartsupp||(function(d) {
               var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
               s=d.getElementsByTagName('script')[0];c=d.createElement('script');
